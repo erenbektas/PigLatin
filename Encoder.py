67d4b3ea439bb55510d7e26a374b4db982b21434
Jenoah <yusuferenbektas@gmail.com>
2022-02-20 19:45:28 +0300

    Sentence Support (Beta 2)
    Sentence support.
    First letter capitalized if it's also capital in the input.

--- Encoder.py
+++ Encoder.py
@@ -1,40 +1,29 @@
 import string
-
 vowels = ("a", "e", "i", "o", "u", "A", "E", "I", "O", "U")
 
 def pigger(x):
+    newsent = []     #list of piglatin words
 
-    sentence = x.split()
-    leng = len(sentence)
-    if leng == 1:
-
-        if x[0] in vowels:
-            return x + "yay"
-        else:
-            if len(x) > 1:
-                if x[1] in vowels:
-                    return x[1:] + x[0] + "ay"
-                else:
-                    return x[2:] + x[0:2] + "ay"
-            else:
-                return x + "ay"
-
-    else:
-        newsent = []
-        low = x.lower()
-        lowlist = low.split()
-        for n in lowlist:
-            if len(n) > 1:
-                if n[0] in vowels:
-                    newword = n + "yay"
-                    newsent.append(newword)
-                elif n[1] in vowels:
-                    newword = n[1:] + n[0] + "ay"
-                    newsent.append(newword)
-                else:
-                    newword = n[2:] + n[0:2] + "ay"
-                    newsent.append(newword)
-            else:
-                newword = n + "ay"
+    low = x.lower()
+    lowlist = low.split()
+    for n in lowlist:
+        if len(n) > 1:
+            if n[0] in vowels:
+                newword = n + "yay"
                 newsent.append(newword)
-        return newsent
+            elif n[1] in vowels:
+                newword = n[1:] + n[0] + "ay"
+                newsent.append(newword)
+            else:
+                newword = n[2:] + n[0:2] + "ay"
+                newsent.append(newword)
+        else:
+            newword = n + "ay"
+            newsent.append(newword)
+    if x[0][0] in string.ascii_uppercase:
+        first = newsent[0] + " "
+        final = " ".join(newsent[1:])
+        return first.capitalize() + final
+    else:
+        final = " ".join(newsent)
+        return final
