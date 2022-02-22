7aaff07b68a1b43fd3ff1ff8f7778f8cc4305a31
Jenoah <yusuferenbektas@gmail.com>
2022-02-22 23:19:37 +0300

    Refactoring The Word Length Check

--- Encoder.py
+++ Encoder.py
@@ -7,22 +7,14 @@ def pigger(x):
     low = x.lower()
     lowlist = low.split()
     for n in lowlist:
-        if len(n) > 1:
             if n[0] in vowels:
                 newword = n + "yay"
                 newsent.append(newword)
-            elif n[1] in vowels:
+            elif len(n) > 1 and n[1] in vowels:
                 newword = n[1:] + n[0] + "ay"
                 newsent.append(newword)
             else:
                 newword = n[2:] + n[0:2] + "ay"
-                newsent.append(newword)
-        else:
-            if n[0] not in vowels:
-                newword = n + "ay"
-                newsent.append(newword)
-            else:
-                newword = n + "yay"
                 newsent.append(newword)
     if x[0][0] in string.ascii_uppercase:
         first = newsent[0] + " "
