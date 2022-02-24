1220285324f91a9d13858e0428a50e7275894e5b
Jenoah <yusuferenbektas@gmail.com>
2022-02-24 17:44:28 +0300

    Certain punctuations will stay at the end of the word
    "!", ".", ",", ":", ";", "?"

--- Encoder.py
+++ Encoder.py
@@ -1,5 +1,6 @@
 import string
-vowels = "a", "e", "i", "o", "u", "A", "E", "I", "O", "U"
+vowels = "a", "e", "i", "o", "u", "ü", "ö", "ı" "A", "E", "I", "O", "U", "Ü", "Ö", "İ"
+puncs = "!", ".", ",", ":", ";", "?"
 
 def pigger(x):
     newsent = []     #list of piglatin words
@@ -7,6 +8,19 @@ def pigger(x):
     low = x.lower()
     lowlist = low.split()
     for n in lowlist:
+
+        if n[-1] in puncs:     #if any of the words is ending with a "puncs" list element
+            if n[0] in vowels:
+                newword = n[:-1] + "yay" + n[-1]
+                newsent.append(newword)
+            elif len(n) > 1 and n[1] in vowels:
+                newword = n[1:-1] + n[0] + "ay" + n[-1]
+                newsent.append(newword)
+            else:
+                newword = n[2:-1] + n[0:2] + "ay" + n[-1]
+                newsent.append(newword)
+
+        else:                  #if any of the words is not ending with a "puncs" list element
             if n[0] in vowels:
                 newword = n + "yay"
                 newsent.append(newword)
@@ -16,6 +30,7 @@ def pigger(x):
             else:
                 newword = n[2:] + n[0:2] + "ay"
                 newsent.append(newword)
+
     if x[0][0] in string.ascii_uppercase:
         first = newsent[0] + " "
         final = " ".join(newsent[1:])
