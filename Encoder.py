1ec38794d558242435a8c6a36701823620ca05ca
Jenoah <yusuferenbektas@gmail.com>
2022-02-25 14:22:42 +0300

    #4 solution
    1. some punctuations will stay at the end of the word.
    2. Each word in input which starts with an uppercase letter also start with an uppercase letter in Pig Latin vers.

--- Encoder.py
+++ Encoder.py
@@ -17,8 +17,12 @@ def pigger(x):
                 newword = n[1:-1] + n[0] + "ay" + n[-1]
                 newsent.append(newword)
             else:
-                newword = n[2:-1] + n[0:2] + "ay" + n[-1]
-                newsent.append(newword)
+                if n[1] in puncs:
+                    newword = n[0] + "ay" + n[1]
+                    newsent.append(newword)
+                else:
+                    newword = n[2:-1] + n[0:2] + "ay" + n[-1]
+                    newsent.append(newword)
 
         else:                  #if any of the words is not ending with a "puncs" list element
             if n[0] in vowels:
@@ -31,10 +35,12 @@ def pigger(x):
                 newword = n[2:] + n[0:2] + "ay"
                 newsent.append(newword)
 
-    if x[0][0] in string.ascii_uppercase:
-        first = newsent[0] + " "
-        final = " ".join(newsent[1:])
-        return first.capitalize() + final
-    else:
-        final = " ".join(newsent)
-        return final
+    raw_input_list = x.split() #list of words in input
+    for n in raw_input_list:
+        pos_n = raw_input_list.index(n) #for position of each words of the input
+        if n[0] in string.ascii_uppercase: #if the first letter of that word is uppercase in input
+            newsent[pos_n] = newsent[pos_n].capitalize() #first letter of the corresponding element will also start with an uppercase
+
+    final = " ".join(newsent)
+    print(newsent)
+    return final
